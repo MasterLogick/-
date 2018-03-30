58e1a9e6b44c5ca7cb221640885fe59f2611d929
MasterLogick <vovasst_m@mail.ru>
2018-03-30 22:35:38 +0300

    1F

--- crdo.java
+++ crdo.java
@@ -5,13 +5,12 @@ import java.io.InputStreamReader;
 public class crdo {
 	public static void main(String[] args) {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		int a = 0, b = 0;
+		int a = 0;
 		try {
 			a = Integer.parseInt(bf.readLine());
-			b = Integer.parseInt(bf.readLine());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		System.out.println(a%7+b-1);
+		System.out.println(a+2-((a+2)%2));
 	}
 }
