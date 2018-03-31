1ddfdab448bdf8258c0cd910963f214a5f30485e
MasterLogick <vovasst_m@mail.ru>
2018-03-31 09:31:53 +0300

    2B

--- crdo.java
+++ crdo.java
@@ -5,27 +5,17 @@ import java.io.InputStreamReader;
 public class crdo {
 	public static void main(String[] args) {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		int a = 0;
+		int a = 0,b=0,c=0;
 		try {
 			a = Integer.parseInt(bf.readLine());
+			b = Integer.parseInt(bf.readLine());
+			c = Integer.parseInt(bf.readLine());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		if (a >= 400) {
-			if (a % 400 == 0) {
-				System.out.println("NO");
-				return;
-			}
-		} else {
-			if (a >= 100) {
-				if (a % 100 == 0) {
-					System.out.println("NO");
-					return;
-				}
-			} else if (a % 4 == 0) {
-				System.out.println("YES");
-				return;
-			}
-		}
+		int out = a;
+		if(out<b)out=b;
+		if(out<c)out=c;
+		System.out.println(out);
 	}
 }
