eda0c0546b71acdd072a42cef7a5e811a8329180
MasterLogick <vovasst_m@mail.ru>
2018-03-31 09:58:45 +0300

    2F

--- crdo.java
+++ crdo.java
@@ -5,19 +5,23 @@ import java.io.InputStreamReader;
 public class crdo {
 	public static void main(String[] args) {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		int a = 0, b = 0, c = 0,d = 0;
+		int min = 0, max = 0, x = 0,y = 0;
 		try {
-			a = Integer.parseInt(bf.readLine());
-			b = Integer.parseInt(bf.readLine());
-			c = Integer.parseInt(bf.readLine());
-			d = Integer.parseInt(bf.readLine());
+			min = Integer.parseInt(bf.readLine());
+			max = Integer.parseInt(bf.readLine());
+			x = Integer.parseInt(bf.readLine());
+			y = Integer.parseInt(bf.readLine());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		if(Math.abs(a-c)%2==Math.abs(b-d)%2){
-			System.out.println("YES");
-			return;
+		if(min>max){
+			int tmp=min;
+			min=max;
+			max=tmp;
 		}
-		System.out.println("NO");
+		if(2*x>min)x=min-x;
+		if(2*y>max)y=max-y;
+		if(x>y) System.out.println(y);
+		else System.out.println(x);
 	}
 }
