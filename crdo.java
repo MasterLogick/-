35eb12c650d38c6c4442ed48576f66d2ebdbc450
MasterLogick <vovasst_m@mail.ru>
2018-03-31 10:19:35 +0300

    2G

--- crdo.java
+++ crdo.java
@@ -5,23 +5,29 @@ import java.io.InputStreamReader;
 public class crdo {
 	public static void main(String[] args) {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		int min = 0, max = 0, x = 0,y = 0;
+		//         1      5      10     20     60
+		int n = 0, a = 0, b = 0, c = 0, d = 0, e = 0;
 		try {
-			min = Integer.parseInt(bf.readLine());
-			max = Integer.parseInt(bf.readLine());
-			x = Integer.parseInt(bf.readLine());
-			y = Integer.parseInt(bf.readLine());
-		} catch (IOException e) {
-			e.printStackTrace();
+			n = Integer.parseInt(bf.readLine());
+		} catch (IOException ex) {
+			ex.printStackTrace();
 		}
-		if(min>max){
-			int tmp=min;
-			min=max;
-			max=tmp;
+		if (n > 60) {
+			e = (n - n % 60) / 60;
+			n %= 60;
 		}
-		if(2*x>min)x=min-x;
-		if(2*y>max)y=max-y;
-		if(x>y) System.out.println(y);
-		else System.out.println(x);
+		if (n > 20) {
+			d = (n - n % 20) / 20;
+			n %= 20;
+		}
+		if (n > 10) {
+			c = (n - n % 10) / 10;
+			n %= 10;
+		}
+		if (n > 5) {
+			b = (n - n % 5) / 5;
+			a = n % 5;
+		}
+		System.out.println(a + " " + b + " " + c + " " + d + " " + e);
 	}
 }
