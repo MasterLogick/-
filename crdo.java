92b181218e1d8686f5d6be85f965e6acedd0bb44
MasterLogick <vovasst_m@mail.ru>
2018-03-31 09:43:46 +0300

    2C

--- crdo.java
+++ crdo.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 public class crdo {
 	public static void main(String[] args) {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		int a = 0,b=0,c=0;
+		int a = 0, b = 0, c = 0;
 		try {
 			a = Integer.parseInt(bf.readLine());
 			b = Integer.parseInt(bf.readLine());
@@ -13,9 +13,17 @@ public class crdo {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		int out = a;
-		if(out<b)out=b;
-		if(out<c)out=c;
-		System.out.println(out);
+		if (a == b && b == c) {
+			System.out.println(3);
+			return;
+		}
+		//  a b c
+		//  1 1 2         1 2 1         2 1 1
+		//  1  1||1  2    1  1||1  2    1  1||1 2
+		if((a==b&&b!=c)||(a==c&&a!=b)||(c==b&&c!=a)){
+			System.out.println(2);
+			return;
+		}
+		System.out.println(0);
 	}
 }
