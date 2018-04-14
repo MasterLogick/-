92032313742794d06371c2589a531ec2040c6bde
MasterLogick <vovasst_m@mail.ru>
2018-04-14 20:31:17 +0300

    3F

--- crdo.java
+++ crdo.java
@@ -12,27 +12,16 @@ public class crdo {
         } catch (IOException ex) {
             ex.printStackTrace();
         }
-        int counter=0;
-        int step = 1;
-        for (int i = 0; i <a ; i++) {
-            if(counter<step){
-                counter++;
-                if(i+1==a){
-                    System.out.print(step);
-                    return;
-                }else{
-                    System.out.print(step+" ");
-                }
-            }else{
-                step++;
-                counter=1;
-                if(i+1==a){
-                    System.out.print(step);
-                    return;
-                }else{
-                    System.out.print(step+" ");
-                }
+        int counter=0,sum=0;
+        while(a!=0){
+            sum+=a;
+            counter++;
+            try {
+                a = Integer.parseInt(bf.readLine());
+            } catch (IOException ex) {
+                ex.printStackTrace();
             }
         }
+        System.out.println(((double) sum)/((double)counter));
     }
 }
