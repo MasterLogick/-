e2327ff0800e24689b083efd8aef61cac26fcb75
MasterLogick <vovasst_m@mail.ru>
2018-04-14 19:37:53 +0300

    3B

--- crdo.java
+++ crdo.java
@@ -1,28 +1,22 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 
 public class crdo {
     public static void main(String[] args) {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        //         1      5      10     20     60
-        int a=0,b=0;
+        int a=0;
         try {
             a = Integer.parseInt(bf.readLine());
-            b = Integer.parseInt(bf.readLine());
         } catch (IOException ex) {
             ex.printStackTrace();
         }
-        if(a<=b){
-            for (int i = a; i <b; i++) {
-                System.out.print(i+" ");
-            }
-            System.out.println(b);
-        }else{
-            for (int i = a; i >b; i--) {
-                System.out.print(i+" ");
-            }
-            System.out.println(b);
+        BigInteger bi = new BigInteger("0");
+        for (int i = 2; i <a ; i++) {
+            System.out.print(i-1+"*"+i+"+");
+            bi=bi.add(new BigInteger(Integer.toString(i)).multiply(new BigInteger(Integer.toString(i-1))));
         }
+        System.out.println(a-1+"*"+a+"="+bi.add(new BigInteger(Integer.toString(a)).multiply(new BigInteger(Integer.toString(a-1)))).toString());
     }
 }
