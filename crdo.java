0712406dfb35aa7ed09af53f743e4cee5190f171
MasterLogick <vovasst_m@mail.ru>
2018-04-14 19:32:38 +0300

    3A

--- crdo.java
+++ crdo.java
@@ -6,18 +6,23 @@ public class crdo {
     public static void main(String[] args) {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         //         1      5      10     20     60
-        int k = 0, m = 0, n = 0;
+        int a=0,b=0;
         try {
-            k = Integer.parseInt(bf.readLine());
-            m = Integer.parseInt(bf.readLine());
-            n = Integer.parseInt(bf.readLine());
+            a = Integer.parseInt(bf.readLine());
+            b = Integer.parseInt(bf.readLine());
         } catch (IOException ex) {
             ex.printStackTrace();
         }
-        if (n <= k) {
-            System.out.println(2 * m);
-        } else if (n * 2 % k == 0) {
-            System.out.println(m * n * 2 / k);
-        } else System.out.println(m * (1 + n * 2 / k));
+        if(a<=b){
+            for (int i = a; i <b; i++) {
+                System.out.print(i+" ");
+            }
+            System.out.println(b);
+        }else{
+            for (int i = a; i >b; i--) {
+                System.out.print(i+" ");
+            }
+            System.out.println(b);
+        }
     }
 }
