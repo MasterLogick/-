ff5cea1982ae07de0f6b0791466abd20cb9c4135
MasterLogick <vovasst_m@mail.ru>
2018-04-16 21:23:25 +0300

    4I

--- crdo.java
+++ crdo.java
@@ -2,41 +2,45 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-import java.util.Vector;
 
 public class crdo {
     public static void main(String[] args) {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        Vector<Integer> a = new Vector<Integer>();
+        int[] a = new int[10];
         try {
-            bf.readLine();
             String s = bf.readLine();
             StringTokenizer st = new StringTokenizer(s, " ");
-            while (st.hasMoreElements()) {
-                a.add(Integer.parseInt(st.nextToken()));
+            for (int i = 0; i < 10; i++) {
+                a[i] = Integer.parseInt(st.nextToken());
             }
         } catch (IOException ex) {
             ex.printStackTrace();
         }
-        if(a.size()==1){
-            System.out.println(0);
-            return;
-        }
-        if(a.size()==2&&a.get(0)!=a.get(1)){
-            System.out.println(0);
-        }
-        if(a.size()==2&&a.get(0)==a.get(1)){
-            System.out.println(1);
-        }
-        int count = 0;
-        for (int i = 1; i < a.size()-1; i++) {
-
-            if((a.get(i)==a.get(i+1))||(a.get(i)==a.get(i-1))||(a.get(i)>a.get(i-1)&&a.get(i)<a.get(i+1))||(a.get(i)<a.get(i-1)&&a.get(i)>a.get(i+1))){
-                a.remove(i);
-                i--;
-                count++;
+        int max = 0;
+        for (int i = 0; i < 10; i++) {
+            if (a[i] == 0 || a[i] == 2) continue;
+            int left = 0, right = 0;
+            for (int j = i - 1; j >= 0; j--) {
+                if (a[j] == 2) {
+                    left = i - j;
+                    break;
+                }
             }
+
+            for (int j = i + 1; j < 10; j++) {
+                if (a[j] == 2) {
+                    right = j - i;
+                    break;
+                }
+            }
+            if (left == 0)
+                max = Math.max(right, max);
+            else if (right == 0)
+                max = Math.max(left, max);
+
+            else
+                max = Math.max(Math.min(left, right), max);
         }
-        System.out.println(count);
+        System.out.println(max);
     }
 }
