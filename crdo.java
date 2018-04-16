b6aa4dd84def7462a0ec038e6ecbd931d651992e
MasterLogick <vovasst_m@mail.ru>
2018-04-16 17:44:20 +0300

    4A

--- crdo.java
+++ crdo.java
@@ -1,27 +1,30 @@
+import sun.java2d.SurfaceDataProxy;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigInteger;
+import java.util.StringTokenizer;
+import java.util.Vector;
 
 public class crdo {
     public static void main(String[] args) {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int a=0;
+        Vector<Integer> a = new Vector<Integer>();
         try {
-            a = Integer.parseInt(bf.readLine());
+            bf.readLine();
+            String s = bf.readLine();
+            StringTokenizer st = new StringTokenizer(s," ");
+            while (st.hasMoreElements()){
+                a.add(Integer.parseInt(st.nextToken()));
+            }
         } catch (IOException ex) {
             ex.printStackTrace();
         }
-        int counter=0,sum=0;
-        while(a!=0){
-            sum+=a;
-            counter++;
-            try {
-                a = Integer.parseInt(bf.readLine());
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
+        int count =0;
+        for (int i = 1; i <a.size(); i++) {
+            if(a.get(i)>a.get(i-1))count++;
         }
-        System.out.println(((double) sum)/((double)counter));
+        System.out.println(count);
     }
 }
