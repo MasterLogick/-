64e3d4b7d4da1a7792cc697b577bf2e9cd44d6d2
MasterLogick <vovasst_m@mail.ru>
2018-04-16 18:42:00 +0300

    4D

--- crdo.java
+++ crdo.java
@@ -1,11 +1,6 @@
-import sun.java2d.SurfaceDataProxy;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
-import java.math.BigInteger;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 import java.util.Vector;
 
@@ -13,24 +8,46 @@ public class crdo {
     public static void main(String[] args) {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         Vector<Integer> a = new Vector<Integer>();
-        int height=0;
         try {
-            bf.readLine();
+            //bf.readLine();
             String s = bf.readLine();
-            StringTokenizer st = new StringTokenizer(s," ");
-            while (st.hasMoreElements()){
+            StringTokenizer st = new StringTokenizer(s, " ");
+
+
+            st.nextToken();//-------------------------------------------------------------------------------------------------
+
+
+            while (st.hasMoreElements()) {
                 a.add(Integer.parseInt(st.nextToken()));
             }
-            height=Integer.parseInt(bf.readLine());
         } catch (IOException ex) {
             ex.printStackTrace();
         }
-        for (int i = 0; i <a.size(); i++) {
-            if(a.get(i)<height){
-                System.out.println(i+1);
-                return;
+        int count = 0;
+        boolean f = false;
+        while (true) {
+            for (int i = 1; i < a.size() - 1; i++) {
+                if (a.get(i) == a.get(i - 1) && a.get(i) == a.get(i + 1)) {
+                    int tmp = i + 1;
+                    for (; tmp < a.size(); tmp++) {
+                        if (a.get(tmp - 1) != a.get(tmp)) {
+                            tmp--;
+                            break;
+                        }
+                    }
+                    count += tmp - i + 2;
+                    for (int j = 0; j < tmp - i + 2; j++) {
+                        a.remove(i-1);
+                    }
+                    f = true;
+                    break;
+                }
             }
+            if (!f) {
+                System.out.println(count);
+                return;
+            } else
+                f = false;
         }
-        System.out.println(a.size()+1);
     }
 }
