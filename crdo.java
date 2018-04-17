96244ab7b406249e1bd9d38b51459949ffa862c3
MasterLogick <vovasst_m@mail.ru>
2018-04-17 21:42:21 +0300

    4H

--- crdo.java
+++ crdo.java
@@ -1,58 +1,86 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 import java.util.Vector;
 
 public class crdo {
     public static void main(String[] args) {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         Vector<Integer> a = new Vector<Integer>();
+        Vector<Boolean> b = new Vector<Boolean>();
         try {
-            //bf.readLine();
-            String s = bf.readLine();
-            StringTokenizer st = new StringTokenizer(s, " ");
-
-
-            st.nextToken();//-------------------------------------------------------------------------------------------------
-
-
-            while (st.hasMoreElements()) {
-                a.add(Integer.parseInt(st.nextToken()));
+            int n = Integer.parseInt(bf.readLine());
+            for (int i = 0; i < n; i++) {
+                a.add(Integer.parseInt(bf.readLine()));
+                b.add(false);
             }
         } catch (IOException ex) {
             ex.printStackTrace();
         }
-        int count = 0;
-        boolean f = false;
-        while (true) {
-            for (int i = 1; i < a.size() - 1; i++) {
-                if (a.get(i) == a.get(i - 1) && a.get(i) == a.get(i + 1)) {
-                    int tmp = i + 1;
-                    for (; tmp < a.size(); tmp++) {
-                        if (a.get(tmp - 1) != a.get(tmp)) {
-                            tmp--;
-                            break;
-                        }
-                    }
-                    if(tmp==a.size())tmp--;
-                    count += tmp - i + 2;
-                    for (int j = 0; j < tmp - i + 2; j++) {
-                        //try {
-                        a.remove(i - 1);
-                        /*} catch (ArrayIndexOutOfBoundsException ex) {
-
-                        }*/
-                    }
-                    f = true;
-                    break;
+        if (a.size() == 1) {
+            System.out.println(a.get(0));
+            return;
+        }
+        int tmp = 0;
+        for (int i = 0; i < a.size(); i++) {
+            if (b.get(i) || a.get(i) != 5) continue;
+            try {
+                if (a.get(i - 1) < 5) {
+                    b.set(i - 1, true);
+                    a.set(i - 1, 5);
                 }
+            } catch (ArrayIndexOutOfBoundsException ex) {
+
             }
-            if (!f) {
-                System.out.println(count);
-                return;
-            } else
-                f = false;
+            try {
+                if (a.get(i + 1) < 5) {
+                    b.set(i + 1, true);
+                    a.set(i + 1, 5);
+                }
+            } catch (ArrayIndexOutOfBoundsException ex) {
+
+            }
+        }
+        for (int i = 0; i < a.size(); i++) {
+            if (b.get(i) || a.get(i) != 4) continue;
+            try {
+                if (a.get(i - 1) < 4) {
+                    b.set(i - 1, true);
+                    a.set(i - 1, 4);
+                }
+            } catch (ArrayIndexOutOfBoundsException ex) {
+
+            }
+            try {
+                if (a.get(i + 1) < 4) {
+                    b.set(i + 1, true);
+                    a.set(i + 1, 4);
+                }
+            } catch (ArrayIndexOutOfBoundsException ex) {
+
+            }
+        }
+        for (int i = 0; i < a.size(); i++) {
+            if (b.get(i) || a.get(i) != 3) continue;
+            try {
+                if (a.get(i - 1) < 3) {
+                    b.set(i - 1, true);
+                    a.set(i - 1, 3);
+                }
+            } catch (ArrayIndexOutOfBoundsException ex) {
+
+            }
+            try {
+                if (a.get(i + 1) < 4) {
+                    b.set(i + 1, true);
+                    a.set(i + 1, 3);
+                }
+            } catch (ArrayIndexOutOfBoundsException ex) {
+
+            }
+        }
+        for (int i = 0; i < a.size(); i++) {
+            System.out.println(a.get(i));
         }
     }
 }
